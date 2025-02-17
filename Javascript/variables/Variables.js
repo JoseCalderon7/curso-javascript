b4fe614f7243ead4012fc843ad30942884b83c44
José Calderón <tutuyosoy1111@gmail.com>
2025-02-17 18:15:40 -0300

    Avances del curso.

---
+++ Javascript/variables/Variables.js
@@ -0,0 +1,31 @@
+// FORMA DESCONTINUADA | NO UTILIZAR
+var miNumero = 15
+
+// FORMA NORMAL Y CAMBIABLE PARA INSTANCIAR VARIABLES
+let miNumero2 = 15
+
+// FORMA CONTINUA DE MANTENER EL MISMO FORMATO DE UNA VARIABLE
+const miNumero3 = 2
+
+
+//* REGLAS PARA NOMBRES DE VARIABLES
+/*
+! DEBE COMENZAR CON:
+* Una letra (A-Z o a-z)
+* Un signo de dolar ($)
+* O con un guión bajo (_)
+
+! LO SIGUIENTE:
+* Letras (A-Z o a-z)
+* Digitos (0-9)
+* Guiones bajos (_)
+* Signo de dolar ($)
+
+*/ 
+
+//? JAVASCRIPT ES SENSIBLE A "MAYUSCULAS" Y "MINUSCULAS"
+//? miNombre =/ minombre | SON DIFERENTES
+
+
+//* BUENAS PRÁCTICAS:
+//* ESCRIBIR LAS VARIABLES EN camelCase
