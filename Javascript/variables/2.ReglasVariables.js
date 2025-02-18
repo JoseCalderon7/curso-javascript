70fb7c250a765b357abdddd98ffc378834a5f8bf
José Calderón <tutuyosoy1111@gmail.com>
2025-02-18 13:40:01 -0300

    Avances del curso.

---
+++ Javascript/variables/2.ReglasVariables.js
@@ -0,0 +1,10 @@
+//* NOMBRES VÁLIDOS PARA CREAR UNA VARIABLE.
+
+let $miNumero = 10
+console.log($miNumero);
+
+let _miNumero2 = 20
+console.log(_miNumero2);
+
+let miNumero3 = 30
+console.log(miNumero3);
---
+++ Javascript/variables/3.TipoDatos.js
@@ -0,0 +1,31 @@
+/*
+* Number    = Almacena valores numericos
+* String    = Almacena caracteres
+* Boolean   = True / False
+* Null      = Ausencia de referencia
+* Undefine  = Ausencia de valor (Está vacío)
+
+*/
+
+
+//* TIPOS DE DATOS - "PRIMITIVOS"
+
+//? NUMBER
+let miNumero = 10
+console.log(miNumero);
+
+//? STRING
+let miLetra = 'Hola'
+console.log(miLetra);
+
+//? BOOLEAN
+let miBoolean = true
+console.log(miBoolean);
+
+//? NULL | Valor y tipo de dato
+let miNull = null
+console.log(miNull);
+
+//? UNDEFINE | No tiene definido su valor.
+let miUndefine = undefined
+console.log(miUndefine);
