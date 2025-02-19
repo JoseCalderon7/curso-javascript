990b646dbfb43166c0539d625547df14d15f10b6
José Calderón <tutuyosoy1111@gmail.com>
2025-02-19 17:17:17 -0300

    Avances del curso.

---
+++ Javascript/variables/5.Hoisting.js
@@ -0,0 +1,14 @@
+//* HOISTING = Las declaraciones de una variable pueden estar en cualquier parte.
+//! AL USAR "let" NO APLICA CONCEPTO DE HOISTING.
+
+'use strict' // Desactiva el Hoisting.
+// Significa que no podemos dejar variables sin declarar previamente.
+
+
+//? EJEMPLO:
+
+x = 10
+
+console.log(x);
+
+let x
---
+++ Javascript/variables/6.Constantes.js
@@ -0,0 +1,12 @@
+//* UNA CONSTANTE QUE NO PUEDE CAMBIAR SU VALOR Y PROTEGE EL VALOR ORIGINAL.
+
+//* Una constante se define en MAYUSCULAS Y DEBE inicializarse INMEDIATAMENTE una vez se crea. 
+//! const MI_CONSTANTE = 10; || const SEGUNDOS_POR_MINUTO = 60;
+
+
+// No puede cambiar su valor
+const MI_CONSTANTE = 10;
+
+
+// Ejemplo de constante en JavaScript
+console.log(Math.PI);
