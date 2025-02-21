42058adad5cffcbe0d8cb8b3756823ac00ae0101
José Calderón <tutuyosoy1111@gmail.com>
2025-02-21 20:17:13 -0300

    Avances del curso.

---
+++ Javascript/2.Operadores/1.OperadoresJS.js
@@ -0,0 +1,14 @@
+//* OPERADORES ARITMÉTICOS
+// Acá tenemos todos los tipos que podemos encontrar para realizar operaciones matemáticas dentro de nuestro código de JavaScript.
+
+/*
+? SUMA +        | x + y
+? RESTA -       | x - y
+? MULTIPLICACIÓN * | x * y
+? DIVISIÓN /    | x / y
+? MÓDULO (%)    | x % y
+? POTENCIA **   | x ** y
+? INCREMENTO ++ | ++x ó x++
+? DECREMENTO -- | --y ó y--
+
+*/
