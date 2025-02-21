ed2a100d9c681312491ca7c785a51da5e3154ccc
José Calderón <tutuyosoy1111@gmail.com>
2025-02-20 23:57:48 -0300

    Avances del curso.

--- Javascript/variables/6.Constantes.js
+++ Javascript/variables/6.Constantes.js
@@ -3,10 +3,10 @@
 //* Una constante se define en MAYUSCULAS Y DEBE inicializarse INMEDIATAMENTE una vez se crea. 
 //! const MI_CONSTANTE = 10; || const SEGUNDOS_POR_MINUTO = 60;
 
-
 // No puede cambiar su valor
 const MI_CONSTANTE = 10;
 
-
 // Ejemplo de constante en JavaScript
 console.log(Math.PI);
+
+// Ideal para datos fijos en el tiempo.
