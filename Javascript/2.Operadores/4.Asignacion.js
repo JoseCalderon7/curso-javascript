d821b03fce4b3f836d4f158fb21980c284ac074f
José Calderón <tutuyosoy1111@gmail.com>
2025-02-22 17:52:36 -0300

    Avances del curso.

---
+++ Javascript/2.Operadores/4.Asignacion.js
@@ -0,0 +1,35 @@
+//* OPERADORES DE ASIGNACIÓN
+
+//* Asignación básica
+x = 5;
+
+//* Asignación compuesto de suma
+x += 3;
+
+//? ES IGUAL A:
+x = x + 3
+
+//----------- EJEMPLOS:
+let a = 5;
+a += 3;  // Equivale a: a = a + 3;
+console.log(a); // 8
+
+let b = 10;
+b -= 4;  // Equivale a: b = b - 4;
+console.log(b); // 6
+
+let c = 6;
+c *= 2;  // Equivale a: c = c * 2;
+console.log(c); // 12
+
+let d = 20;
+d /= 5;  // Equivale a: d = d / 5;
+console.log(d); // 4
+
+let e = 17;
+e %= 3;  // Equivale a: e = e % 3;
+console.log(e); // 2
+
+let f = 4;
+f **= 2;  // Equivale a: f = f ** 2;
+console.log(f); // 16
